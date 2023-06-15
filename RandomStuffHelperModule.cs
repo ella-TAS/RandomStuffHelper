e57ef3f709d0e60b4da9fd4cea55361928cf86d6
EllaTAS <86128357+ella-TAS@users.noreply.github.com>
2023-06-15 18:27:46 +0200

    mod setup

---
+++ RandomStuffHelperModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Celeste.Mod.RandomStuffHelper {
+    public class RandomStuffHelperModule : EverestModule {
+        
+        public static RandomStuffHelperModule Instance;
+
+        public RandomStuffHelperModule() {
+            Instance = this;
+        }
+
+        public override void Load() {
+        }
+
+        public override void Unload() {
+        }
+
+    }
+}
